14e03f7067318b7531bd2b11dd2c79c86880078d
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-04-30 20:06:39 -0700

    start the bootstap and jquery date picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,5 +34,11 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+      })
 
+      $(function(){
+        $('#datepicker').datepicker();
+      });
 });
